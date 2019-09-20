8a4ccd06d61a1b53eff315cb9b2f308cb70751d9
Victor <victorbandin@gmail.com>
2019-09-20 19:26:13 +0200

    Create Student Model

---
+++ IOS-Clase-1/Data/Local/Models/Student.swift
@@ -0,0 +1,31 @@
+//
+//  Student.swift
+//  IOS-Clase-1
+//
+//  Created by Dev2 on 20/09/2019.
+//  Copyright © 2019 Vbandin. All rights reserved.
+//
+
+import Foundation
+
+class Student: CustomStringConvertible {
+    // Properties with optional '?' value can be nil
+    var name: String?
+    var email: String?
+    var birthdate: Date?
+    // Use description var of CustomStringConvertible
+    // to print this class with custom String format
+    var description: String {
+        return "Student data: \(String(describing: name)) \(String(describing: email))"
+    }
+
+    // Custom init (Constructor) class with default values
+    convenience init(name:String, email: String? = nil,
+                     birthdate: Date? = nil) {
+        self.init()
+        // always init all propertiess
+        self.name = name
+        self.email = email
+        self.birthdate = birthdate
+    }
+}
